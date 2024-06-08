6c481950b5d589b436f2d807b9d401dd135a03ed
Jamie Coleman <j.a.coleman1978@gmail.com>
2024-06-08 16:08:04 -0500

    Added common d&d variable types.

---
+++ src/staticData/types.ts
@@ -0,0 +1,15 @@
+export type ItemRarity = 'Common' | 'Uncommon' | 'Rare' | 'Very Rare' | 'Legendary';
+
+export type DamageType = 'Bludgeoning' | 'Piercing' | 'Slashing' | 'Magic';
+
+export type CritType = 'Hit' | 'Miss';
+
+export type Level = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
+
+export type SpellLevel = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+export type AdvantageType = 'Normal' | 'Advantage' | 'Disadvantage';
+
+export type SaveModifier = -5 | -4 | -3 | -2 | -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
+
+export type DiceType = 'd4' | 'd6' | 'd8' | 'd10' | 'd12' | 'd20' | 'd100';
