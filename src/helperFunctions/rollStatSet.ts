ac093b5548b318a7921810abe07ffacc70a3377d
Jamie Coleman <j.a.coleman1978@gmail.com>
2024-06-11 12:13:48 -0500

    Added function headers for rollCriticals.ts

--- src/helperFunctions/rollStatSet.ts
+++ src/helperFunctions/rollStatSet.ts
@@ -1,8 +1,19 @@
 import { rollDice } from "./rollDice";
 
+/**
+ * Rolls a single stat using 4d6 drop lowest method, ensuring that all stats are at least a certain value.
+ * @param minimumStatAllowed An integer between 3 and 18 representing the minimum value allowed for a stat, with a default value of 3.
+ * @returns A single stat between 3 and 18.
+ */
 export const rollStat = (minimumStatAllowed: number = 3): number => {
-    if (minimumStatAllowed < 3) throw new Error('Minimum stat allowed must be 3 or greater for rollStat.');
-    if(minimumStatAllowed > 18) throw new Error('Minimum stat allowed must be 18 or less for rollStat.');
+    if (minimumStatAllowed < 3)
+        throw new Error(
+            "Minimum stat allowed must be 3 or greater for rollStat."
+        );
+    if (minimumStatAllowed > 18)
+        throw new Error(
+            "Minimum stat allowed must be 18 or less for rollStat."
+        );
 
     let total = 0;
 
@@ -20,20 +31,49 @@ export const rollStat = (minimumStatAllowed: number = 3): number => {
     return total;
 };
 
-
-export const rollStatSet = (minimumStatAllowed: number = 3, minimumStatSum: number = 18, atLeastOneStatEqualToOrGreaterThan: number = 3): number[] => {
-    if (minimumStatAllowed < 3) throw new Error('Minimum stat allowed must be 3 or greater for rollStatSet.');
-    if(minimumStatAllowed > 18) throw new Error('Minimum stat allowed must be 18 or less for rollStatSet.');
-    if (minimumStatSum < 18) throw new Error('Minimum stat sum must be 18 or greater for rollStatSet.');
-    if(minimumStatSum > 108) throw new Error('Minimum stat sum must be 108 or less for rollStatSet.');
-    if(atLeastOneStatEqualToOrGreaterThan < 3) throw new Error('Minimum stat allowed must be 3 or greater for rollStatSet.');
-    if(atLeastOneStatEqualToOrGreaterThan > 18) throw new Error('Minimum stat allowed must be 18 or less for rollStatSet.');
+/**
+ * Rolls a set of 6 stats using 4d6 drop lowest method, ensuring that all stats are at least a certain value, that the sum of the stats is at least a certain value and that at least one of the stats is or exceeds a certain value.
+ * @param minimumStatAllowed An integer between 3 and 10 representing the minimum value allowed for a stat, with a default value of 3.
+ * @param minimumStatSum An integer between 18 and 85 representing the minimum sum of the stats, with a default value of 18.
+ * @param atLeastOneStatEqualToOrGreaterThan An integer between 3 and 18 representing the minimum value for at least one stat, with a default value of 3.
+ * @returns An array of 6 stats between 3 and 18.
+ */
+export const rollStatSet = (
+    minimumStatAllowed: number = 3,
+    minimumStatSum: number = 18,
+    atLeastOneStatEqualToOrGreaterThan: number = 3
+): number[] => {
+    if (minimumStatAllowed < 3)
+        throw new Error(
+            "Minimum stat allowed must be 3 or greater for rollStatSet."
+        );
+    if (minimumStatAllowed > 11)
+        throw new Error(
+            "Minimum stat allowed must be 10 or less for rollStatSet."
+        );
+    if (minimumStatSum < 18)
+        throw new Error(
+            "Minimum stat sum must be 18 or greater for rollStatSet."
+        );
+    if (minimumStatSum > 85)
+        throw new Error("Minimum stat sum must be 85 or less for rollStatSet.");
+    if (atLeastOneStatEqualToOrGreaterThan < 3)
+        throw new Error(
+            "Minimum stat allowed must be 3 or greater for rollStatSet."
+        );
+    if (atLeastOneStatEqualToOrGreaterThan > 18)
+        throw new Error(
+            "Minimum stat allowed must be 18 or less for rollStatSet."
+        );
 
     let total = 0;
     let statSet: number[] = [];
     let maxStat = 0;
 
-    while (total < minimumStatSum || maxStat < atLeastOneStatEqualToOrGreaterThan) {
+    while (
+        total < minimumStatSum ||
+        maxStat < atLeastOneStatEqualToOrGreaterThan
+    ) {
         statSet = [];
         total = 0;
         for (let i = 0; i < 6; i++) {
@@ -46,5 +86,5 @@ export const rollStatSet = (minimumStatAllowed: number = 3, minimumStatSum: numb
         maxStat = Math.max(...statSet);
     }
 
-    return statSet
+    return statSet;
 };
